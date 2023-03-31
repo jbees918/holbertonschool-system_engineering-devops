2bce789f5bfc5ffcaae49879be266dce7b229b5f
jbees918 <josh.beestoned@gmail.com>
2023-03-30 17:17:55 -0700

    task0

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Module to retrieve TODO list progress of a given employee ID from a
+REST API.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("Usage: {} EMPLOYEE_ID".format(argv[0]))
+        exit(1)
+
+    EMPLOYEE_ID = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        EMPLOYEE_ID)
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Error: Request failed with status code {}".format(
+            response.status_code))
+        exit(1)
+
+    todos = response.json()
+    total_tasks = len(todos)
+    done_tasks = [todo for todo in todos if todo['completed']]
+
+    employee_name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        EMPLOYEE_ID)
+    response_name = requests.get(employee_name_url)
+
+    if response_name.status_code != 200:
+        print("Error: Request failed with status code {}".format(
+            response_name.status_code))
+        exit(1)
+
+    employee_name = response_name.json().get('name')
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(done_tasks), total_tasks))
+
+    for todo in done_tasks:
+        print("\t {} {}".format('\t', todo['title']))
